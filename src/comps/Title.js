2edf96e5dd9cff82be90bf654aff7d1600e0dc60
Vivek Lokhande <vivek2811999@gmail.com>
2021-04-23 21:29:50 +0530

    minor changes in title

--- src/comps/Title.js
+++ src/comps/Title.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Title = () => {
   return (
     <div className="title">
-      <h1>CityGram</h1>
+      <h1>CityStreets</h1>
       <h2>CityStreets</h2>
       <p>Share photo's of your city streets!!</p>
     </div>
